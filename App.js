4548a09c17d62bdc78842e719539ed49b7be5146
Jille Bonfemme <jilleb@jakku.edu>
2020-04-08 17:15:01 +0200

    finish

--- App.js
+++ App.js
@@ -1,113 +1,132 @@
-import React, { Component } from "react";
-import { StyleSheet, Alert, SafeAreaView, Button, Text, TextInput, View } from "react-native";
-import OptionsMenu from "react-native-options-menu";
-const MoreIcon = require("../AwesomeProject - Copy/61140.png");
-export default class PizzaTranslator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { num1: "", num2: "", result: "" };
+import React from "react";
+import { ScrollView, StyleSheet, Text, View, TextInput } from "react-native";
+import Constants from "expo-constants";
+import * as SQLite from "expo-sqlite";
+
+const db = SQLite.openDatabase("database");
+
+export default class employee extends React.Component {
+  state = {
+    text: "",
+    employees: null
+  };
+
+  onUpgrade = () => {
+    db.transaction(tx => {
+      tx.executeSql("drop table department", []);
+      tx.executeSql("drop table employee", []);
+    });
+  };
+
+  componentDidMount() {
+    this.onUpgrade();
+    db.transaction(tx => {
+      tx.executeSql(
+        "create table if not exists employee (EmpID integer primary key not null, name text not null, title text not null, phone text not null, email text not null, Dept_id integer, FOREIGN KEY(Dept_id) REFERENCES Department(DeptID));"
+      );
+    });
+
+    db.transaction(tx => {
+      tx.executeSql("create table if not exists department (DepID integer primary key not null, name text);");
+    });
+
+    this.onCreate();
   }
-  sum() {
-    // console.log(this.state.num1 + "hahah");
-    var n1 = parseFloat(this.state.num1);
-    var n2 = parseFloat(this.state.num2);
-    this.setState({ result: String(n1 + n2) });
-  }
-  subtract() {
-    var n1 = parseFloat(this.state.num1);
-    var n2 = parseFloat(this.state.num2);
-    this.setState({ result: String(n1 - n2) });
-  }
-  mult() {
-    var n1 = parseFloat(this.state.num1);
-    var n2 = parseFloat(this.state.num2);
-    this.setState({ result: String(n1 * n2) });
-  }
-  div() {
-    var n1 = parseFloat(this.state.num1);
-    var n2 = parseFloat(this.state.num2);
-    if (n2 != 0) this.setState({ result: String(n1 / n2) });
-    else alert("can`t divide by zero ");
-  }
+
+  onCreate = () => {
+    db.transaction(tx => {
+      tx.executeSql("insert into department (name) values (?)", ["Sales"]);
+      tx.executeSql("insert into department (name) values (?)", ["Management"]);
+
+      tx.executeSql("select * from department", [], (_, { rows }) => console.log(JSON.stringify(rows._array)));
+    });
+
+    db.transaction(tx => {
+      tx.executeSql("insert into employee (name, title, phone, email, Dept_id) values (?,?,?,?,?)", ["michael", "sales", "012", "michael.com", 1]);
+      tx.executeSql("insert into employee (name, title, phone, email, Dept_id) values (?,?,?,?,?)", ["Mohamed", "sales", "012", "michael.com", 1]);
+      tx.executeSql("insert into employee (name, title, phone, email, Dept_id) values (?,?,?,?,?)", ["ahmed", "sales", "012", "michael.com", 1]);
+      tx.executeSql("insert into employee (name, title, phone, email, Dept_id) values (?,?,?,?,?)", ["soso", "sales", "012", "michael.com", 1]);
+      tx.executeSql("insert into employee (name, title, phone, email, Dept_id) values (?,?,?,?,?)", ["koko", "sales", "012", "michael.com", 1]);
+      tx.executeSql("insert into employee (name, title, phone, email, Dept_id) values (?,?,?,?,?)", ["hoho", "manager", "012", "michael.com", 2]);
+      tx.executeSql("insert into employee (name, title, phone, email, Dept_id) values (?,?,?,?,?)", ["hjhj", "manager", "012", "michael.com", 2]);
+
+      tx.executeSql("select * from employee", [], (_, { rows }) => console.log(JSON.stringify(rows)));
+    });
+  };
+
+  getEmployees = query => {
+    db.transaction(tx => {
+      tx.executeSql("select * from employee where name = ?", [query], (_, { rows }) => {
+        if (rows.length > 0) {
+          this.setState({ employees: rows._array });
+        } else this.setState({ employees: null });
+      });
+    });
+  };
+
   render() {
     return (
-      <SafeAreaView style={styles.container}>
-        <View style={styles.opt}>
-          <OptionsMenu
-            button={MoreIcon}
-            buttonStyle={{
-              width: 100,
-              height: 20,
-              margin: 7.5,
-              resizeMode: "contain"
-            }}
-            destructiveIndex={1}
-            options={["Sum", "subtract", "multiple", "divide"]}
-            actions={[this.sum.bind(this), this.subtract.bind(this), this.mult.bind(this), this.div.bind(this)]}
-          />
-        </View>
-        <View style={styles.inputs}>
+      <View style={styles.container}>
+        <Text style={styles.heading}>Enter Employee Name</Text>
+        <View style={styles.flexRow}>
           <TextInput
-            keyboardType={"numeric"}
-            placeholder="Number 1"
-            placeholderTextColor="black"
-            // onChangeText={(t) => this.setState({ text1: t, text2: this.state.text2 })}
-            onChangeText={value => {
-              if (!isNaN(value)) {
-                this.setState({ num1: value });
-              }
+            onChangeText={text => this.setState({ text })}
+            onSubmitEditing={() => {
+              this.getEmployees(this.state.text);
+              this.setState({ text: "" });
             }}
-            value={`${this.state.num1}`}
+            placeholder="Search Query"
+            style={styles.input}
+            value={String(this.state.text)}
           />
         </View>
-        <View style={styles.inputs}>
-          <TextInput
-            keyboardType={"numeric"}
-            placeholder="Number 2"
-            placeholderTextColor="black"
-            //   onChangeText={(t) => this.setState({ text1: this.state.text1, text2: t })}
-            onChangeText={value => {
-              if (!isNaN(value)) {
-                this.setState({ num2: value });
-              }
-            }}
-            value={`${this.state.num2}`}
-          />
-        </View>
-        <View style={styles.output}>
-          <Text>{this.state.result}</Text>
-        </View>
-      </SafeAreaView>
+        <ScrollView style={styles.listArea}>
+          {this.state.employees !== null ? (
+            this.state.employees.map((employee, key) => (
+              <View key={key} style={styles.sectionContainer}>
+                <Text>{employee.name}</Text>
+                <Text>{employee.title}</Text>
+                <Text>{employee.phone}</Text>
+                <Text>{employee.email}</Text>
+                <Text>{employee.Dept_id}</Text>
+              </View>
+            ))
+          ) : (
+            <View style={styles.sectionContainer}>
+              <Text style={{ textAlign: "center" }}>Nothing To Show</Text>
+            </View>
+          )}
+        </ScrollView>
+      </View>
     );
   }
 }
+
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#fff",
     flex: 1,
-    justifyContent: "flex-end",
-
-    alignItems: "center",
-    marginBottom: 350
+    paddingTop: Constants.statusBarHeight
+  },
+  heading: {
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  flexRow: {
+    flexDirection: "row"
+  },
+  input: {
+    borderColor: "black",
+    borderRadius: 4,
+    borderWidth: 1,
+    flex: 1,
+    height: 48,
+    margin: 16,
+    padding: 8
   },
 
-  inputs: {
-    borderRadius: 10,
-    borderWidth: 2,
-    borderColor: "#009688",
-    padding: 10,
-    backgroundColor: "white",
-    color: "black"
-  },
-  output: {
-    padding: 20
-  },
-
-  buttons: {
-    padding: 10
-  },
-  opt: {
-    position: "absolute",
-    top: 100,
-    right: -10
+  sectionContainer: {
+    marginBottom: 16,
+    marginHorizontal: 16
   }
 });
